7096dbae86e1dc19d7b91cf030178692e9447d3e
nk2ge5k <nk2ge5k@gmail.com>
2016-05-08 18:34:56 +0300

    Changed: basic request

--- Simaland/Client.php
+++ Simaland/Client.php
@@ -3,8 +3,6 @@
 namespace Simaland;
 
 
-use Http\Request;
-
 class Client extends \Http\Client
 {
     /**
@@ -19,6 +17,10 @@ class Client extends \Http\Client
      * @var \Http\Client
      */
     protected $curl;
+    /**
+     * @var string
+     */
+    protected $user_agent = 'nk2ge5k/simaland-api';
 
     /**
      * Client constructor.
@@ -28,22 +30,30 @@ class Client extends \Http\Client
     public function __construct( $login = null, $password = null )
     {
         $this->curl = new \Http\Client();
+        
         $this->login    = $login;
         $this->password = $password;
+        if ( !is_null($login) )
+            $this->setBasicAuthentication( $login, !is_null($password) ? $password : '' );
     }
 
     /**
      * @param $edge
      * @param string $method
      * @param array $params
-     * @return \Simaland\Request
+     * @return Request
      */
     public function request( $edge, $method='get', $params = []) {
         if ( !in_array(strtolower($method), ['get', 'post', 'put', 'delete']) ) {
             throw  new \InvalidArgumentException( sprintf('Unknown method %s', $method) );
         }
         
-        return new \Simaland\Request($edge, $method, $params);
+        $request = new Request($edge, $method, $params);
+        $request
+            ->setOptions($this->options)
+            ->setOption(CURLOPT_USERAGENT, $this->user_agent);
+        
+        return $request;
     }
 
     /**
--- test/test.php
+++ test/test.php
@@ -4,7 +4,8 @@ require_once '../vendor/autoload.php';
 
 $client = new Simaland\Client();
 
-$category = $client->get('category', ['page' => '2']);
+$category = $client->get('category', ['slug' => 'stroitelstvo-i-remont/vodosnabzhenie-i-vodootvedenie']);
+var_dump($category->getBody());
 foreach ( $category as $item ) {
     var_dump($item);
 }
